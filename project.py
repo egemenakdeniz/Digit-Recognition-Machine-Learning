30de4243c2d9e5315ef1cd4f43f3334ed45a7f8a
Egemen <117409203+egemenakdeniz@users.noreply.github.com>
2025-03-02 14:08:13 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,1 +1,36 @@
-Digit Recognition Machine Learning
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+(X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
+
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+X_train = X_train.reshape(-1, 28 * 28)
+X_test = X_test.reshape(-1, 28 * 28)
+
+model = keras.Sequential([
+    layers.Dense(128, activation='relu', input_shape=(784,)),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')  # 10 sınıf için softmax katmanı
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_split=0.2,verbose=1)
+
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print(f"Test Accuracy: {test_acc}")
+
+predictions = model.predict(X_test)
+predicted_labels = np.argmax(predictions, axis=1)
+
+index = 8675
+plt.imshow(X_test[index].reshape(28, 28), cmap='gray')
+plt.title(f"Prediction: {predicted_labels[index]}, Actual: {y_test[index]}")
+plt.show()
